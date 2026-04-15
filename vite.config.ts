402ad7a5642713e4d15ac84c0c353c408d6e4c2d
Gaudin-01 <emmyraph8@gmail.com>
2026-04-15 15:59:51 +0100

    update vite.cofig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import legacy from "@vitejs/plugin-legacy";
-import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   base: "./",
-  // 1. Plugins go in their own array
   plugins: [
     react(),
     tailwindcss(),
-    tsconfigPaths(),
     legacy({
       targets: ['defaults', 'not IE 11']
   }),],
-  // 2. Resolve is a separate top-level property
   resolve: {
     tsconfigPaths: true,
   },
