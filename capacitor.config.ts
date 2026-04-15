ef83839c89764f348df9f4b254acb0fc9f140f42
Gaudin-01 <emmyraph8@gmail.com>
2026-04-15 14:51:16 +0100

    update app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.circuitiq.app',
-  appName: 'CircuitIQ',
-  webDir: 'dist'
+  appId: "com.circuitiq.app",
+  appName: "CircuitIQ",
+  webDir: "dist",
+  server: {
+    androidScheme: "https",
+  },
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
-
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({ registerType: "autoUpdate" }),
-  ],
+  base: "./",
+  // 1. Plugins go in their own array
+  plugins: [react(), tailwindcss(), legacy({
+      targets: ['defaults', 'not IE 11']
+    }), VitePWA({ registerType: "autoUpdate" })],
+  // 2. Resolve is a separate top-level property
   resolve: {
-    alias: {
-      "@": "./src",
-    },
+    tsconfigPaths: true,
   },
 });
