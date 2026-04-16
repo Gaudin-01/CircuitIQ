927f5f93cc351f3e7bcb035e0012f6a5f1434456
Gaudin-01 <emmyraph8@gmail.com>
2026-04-16 09:10:54 +0100

    Fix build by forcing esbuild minifier

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  base: "./",
-  plugins: [
-    react(),
-    tailwindcss(),
-    legacy({
-      targets: ["defaults", "not IE 11"],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
+  // Add this section to solve the Terser error
   build: {
-    minify: "esbuild",
-    chunkSizeWarningLimit: 1000,
+    minify: "esbuild", // Force Vite to use the built-in minifier instead of Terser
+    chunkSizeWarningLimit: 1600, // Increases limit for your large 2685 module build
   },
   resolve: {
     tsconfigPaths: true,
   },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// import legacy from "@vitejs/plugin-legacy";
+
+// export default defineConfig({
+//   base: "./",
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//     legacy({
+//       targets: ["defaults", "not IE 11"],
+//     }),
+//   ],
+//   build: {
+//     minify: "esbuild",
+//     chunkSizeWarningLimit: 1000,
+//   },
+//   resolve: {
+//     tsconfigPaths: true,
+//   },
+// });
