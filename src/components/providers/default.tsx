56452ddbc4e49378be68e7c3b61156f4ddb8ff0e
Gaudin-01 <emmyraph8@gmail.com>
2026-05-22 13:57:54 +0100

    Update Release

--- src/components/providers/default.tsx
+++ src/components/providers/default.tsx
@@ -1,17 +1,42 @@
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexReactClient } from "convex/react";
 import { QueryClientProvider } from "./query-client.tsx";
 import { ThemeProvider } from "./theme.tsx";
 import { Toaster } from "../ui/sonner.tsx";
 import { TooltipProvider } from "../ui/tooltip.tsx";
 
+// This explicitly connects your app to your Convex database!
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
 export function DefaultProviders({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider>
-      <TooltipProvider>
-        <ThemeProvider>
-          <Toaster />
-          {children}
-        </ThemeProvider>
-      </TooltipProvider>
-    </QueryClientProvider>
+    <ConvexAuthProvider client={convex}>
+      <QueryClientProvider>
+        <TooltipProvider>
+          <ThemeProvider>
+            <Toaster />
+            {children}
+          </ThemeProvider>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </ConvexAuthProvider>
   );
 }
+
+// import { QueryClientProvider } from "./query-client.tsx";
+// import { ThemeProvider } from "./theme.tsx";
+// import { Toaster } from "../ui/sonner.tsx";
+// import { TooltipProvider } from "../ui/tooltip.tsx";
+
+// export function DefaultProviders({ children }: { children: React.ReactNode }) {
+//   return (
+//     <QueryClientProvider>
+//       <TooltipProvider>
+//         <ThemeProvider>
+//           <Toaster />
+//           {children}
+//         </ThemeProvider>
+//       </TooltipProvider>
+//     </QueryClientProvider>
+//   );
+// }
